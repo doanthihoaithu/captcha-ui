ddac11560f012570f56f3f0cfd1b78f53a3f81e3
doanthihoaithu <hoaithuxt1998@gmail.com>
2021-04-22 22:17:03 +0700

    Updates

---
+++ precache-manifest.d7e117069de6fabf9b374bb3ea5954bb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b021e5cfba669884821546510121bff",
+    "url": "/captcha-ui/index.html"
+  },
+  {
+    "revision": "cbd1dfb1ce498cc253cc",
+    "url": "/captcha-ui/static/css/2.02d9624e.chunk.css"
+  },
+  {
+    "revision": "e29ca37da0fb077859e6",
+    "url": "/captcha-ui/static/css/3.987f8640.chunk.css"
+  },
+  {
+    "revision": "241df6e86cc5c5ff0542",
+    "url": "/captcha-ui/static/css/main.0a3b77cf.chunk.css"
+  },
+  {
+    "revision": "cbd1dfb1ce498cc253cc",
+    "url": "/captcha-ui/static/js/2.68a6a56e.chunk.js"
+  },
+  {
+    "revision": "e29ca37da0fb077859e6",
+    "url": "/captcha-ui/static/js/3.0277ea02.chunk.js"
+  },
+  {
+    "revision": "241df6e86cc5c5ff0542",
+    "url": "/captcha-ui/static/js/main.01c6d83f.chunk.js"
+  },
+  {
+    "revision": "b6970977ae09eaa4ba42",
+    "url": "/captcha-ui/static/js/runtime~main.6d74f98b.js"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/captcha-ui/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/captcha-ui/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/captcha-ui/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "03d7cee29f2d6da5fff670277c1fc2b6",
+    "url": "/captcha-ui/static/media/bg-01.03d7cee2.jpg"
+  },
+  {
+    "revision": "42118263920899ddb4da0c54452e5b55",
+    "url": "/captcha-ui/static/media/bg-02.42118263.jpg"
+  },
+  {
+    "revision": "3e8039242e517ee7edc05a4e226e0b80",
+    "url": "/captcha-ui/static/media/preloader3.3e803924.gif"
+  }
+]);
